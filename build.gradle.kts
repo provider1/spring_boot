7bf3b6176d3e2e69a4adbe34873c05ce8f44111d
Lenovo <providerz1998@gmail.com>
2023-02-09 11:34:22 +0300

    adding files

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.6.13"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
+}
+
+group = "c.e"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+// https://mvnrepository.com/artifact/org.mariadb.jdbc/mariadb-java-client
+	implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
+
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+//	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	implementation("org.springframework.security:spring-security-crypto:5.7.5")
+
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+// customize authorization sever
+	implementation("org.springframework.cloud:spring-cloud-starter-oauth2:2.2.5.RELEASE")
+//	import("org.springframework.cloud:spring-cloud-dependencies:2022.0.0")
+
+	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:4.0.0")
+
+
+// https://mvnrepository.com/artifact/javax.persistence/javax.persistence-api
+//	implementation("javax.persistence:javax.persistence-api:2.2")
+
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
