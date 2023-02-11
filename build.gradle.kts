f22709e56d5ed6586d2a5c001e33a8a377bd2c9c
provider1 <120079203+provider1@users.noreply.github.com>
2023-02-11 16:31:58 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,11 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	runtimeOnly("com.mysql:mysql-connector-j")
+	//runtimeOnly("com.mysql:mysql-connector-j")
+	
+	// https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
+	implementation("com.mysql:mysql-connector-j:8.0.32")
+
 
 // https://mvnrepository.com/artifact/org.mariadb.jdbc/mariadb-java-client
 	implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
@@ -48,8 +52,8 @@ dependencies {
 // https://mvnrepository.com/artifact/javax.persistence/javax.persistence-api
 //	implementation("javax.persistence:javax.persistence-api:2.2")
 
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
 }
 
